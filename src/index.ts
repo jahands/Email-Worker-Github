812c00f7783d77f1c3d2e5cf4b16966a2fac9ea7
Jacob Hands <jacob@gogit.io>
2023-03-08 09:20:14 -0600

    Add blob/idx

--- src/index.ts
+++ src/index.ts
@@ -269,9 +269,9 @@ async function saveEmailToB2(env: Env, ctx: ExecutionContext, message: EmailMess
 			// Record saved space
 			const savedSpace = emailContent.byteLength - trimmedEmail.byteLength
 			env.DISQUS_SAVED_SPACE.writeDataPoint({
-				blobs: [],
+				blobs: ['bytes'],
 				doubles: [savedSpace],
-				indexes: []
+				indexes: ['bytes']
 			})
 		} catch (e) {
 			if (e instanceof Error) {
